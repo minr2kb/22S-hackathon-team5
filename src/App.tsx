7a5556f9d996d667e91c3658423b0049bc5c1f4d
zerosheepmoo <zerosheepmoo@gmail.com>
2022-04-22 22:46:05 +0900

    auth test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import Auth from "./auth/Auth";
 
 import Home from "./pages/Home";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				{/* <Route path="about" element={<About />} /> */}
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="about" element={<About />} /> */}
+        <Route path="auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
